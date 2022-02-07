c417a746f69146f50ac725af8dd2666834d421f1
Ivan Samorukov <50461542+gubkinbot@users.noreply.github.com>
2022-02-07 20:58:22 +0500

    test push

--- src/run.py
+++ src/run.py
@@ -4,5 +4,5 @@ token=os.environ.get('TOKEN')
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id,"Hello, World!")
+  bot.send_message(message.chat.id,"Hello, World!!!")
 bot.infinity_polling()
