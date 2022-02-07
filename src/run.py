879da51ef705da18d582edb57b30d00c463b71f5
Ivan Samorukov <50461542+gubkinbot@users.noreply.github.com>
2022-02-07 20:27:20 +0500

    upd run.py

--- src/run.py
+++ src/run.py
@@ -1,1 +1,7 @@
-print("Test run bot")
+import telebot
+token='5206536378:AAELOFjmqK_qPY7OGppdnEejFYEgbskNvvo'
+bot=telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.send_message(message.chat.id,"Hello!")
+bot.infinity_polling()
