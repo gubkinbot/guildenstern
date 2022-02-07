6f2c33a86910cfec80f31bbe8159650262fa68c9
oqqa <oqqa.vw@gmail.com>
2022-02-07 22:03:36 +0400

    debug: kek

--- src/run.py
+++ src/run.py
@@ -1,8 +1,9 @@
 import telebot
-import os
+from dotenv import load_dotenv
+env = load_dotenv('.env')
 token='5206536378:AAELOFjmqK_qPY7OGppdnEejFYEgbskNvvo'
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id,f"Hello, World!!!! 5 >{os.environ.get('TOKEN')}< and >{os.getenv('TOKEN')}<")
+  bot.send_message(message.chat.id,f"Hello, World >{env.TOKEN}< , >{env.TOKEN == token}<")
 bot.infinity_polling()
