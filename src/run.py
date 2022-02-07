b8906a43c8f006e5b1a07b4ec8c5b84442352a67
oqqa <oqqa.vw@gmail.com>
2022-02-07 20:16:29 +0400

    chore: kekw

--- src/run.py
+++ src/run.py
@@ -1,7 +1,9 @@
 import telebot
+import os
+
 token='5206536378:AAELOFjmqK_qPY7OGppdnEejFYEgbskNvvo'
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id,"Hello, World!!!")
+  bot.send_message(message.chat.id,f"Hello, World!!! \n{os.environ.get('TOKEN')}")
 bot.infinity_polling()
