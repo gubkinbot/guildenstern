b6edec0b2d07a92fd4ebfb7ac2afdde6b7f41f35
oqqa <oqqa.vw@gmail.com>
2022-02-07 21:30:09 +0400

    debug: test

--- src/run.py
+++ src/run.py
@@ -1,8 +1,8 @@
 import telebot
-
+import os
 token='5206536378:AAELOFjmqK_qPY7OGppdnEejFYEgbskNvvo'
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id,f"Hello, World!!!!")
+  bot.send_message(message.chat.id,f"Hello, World!!!! 5 >{os.environ('TOKEN')}< and >{os.getenv('TOKEN')}<")
 bot.infinity_polling()
