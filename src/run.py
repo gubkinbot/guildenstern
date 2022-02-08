9180f0836145b743c9d9ed7c25626fc01eda0fc5
Askar Yangirov <nyj1337@gmail.com>
2022-02-08 16:53:17 +0300

    Test message from bot
    1st step to anonymous messages between humans

--- src/run.py
+++ src/run.py
@@ -9,4 +9,11 @@ bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
   bot.send_message(message.chat.id,f"Hello, World! :3")
+
+# askar's test some telebot functionality
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+  bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+
 bot.infinity_polling()
