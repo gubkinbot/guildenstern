8283d018f0035686e089e92fda799752978f8253
oqqa <oqqa.vw@gmail.com>
2022-02-09 17:54:03 +0400

    debug: test connect db on server

--- src/run.py
+++ src/run.py
@@ -1,14 +1,18 @@
 import telebot
+from libs.DB_binding import DB_binding
 
 import os
 from dotenv import load_dotenv
 load_dotenv('./.env')
 token = os.environ.get('TOKEN')
 
+a = DB_binding()
+b = a.Sql("SELECT * FROM users;")
+
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id,f"Hello, World! :3")
+  bot.send_message(message.chat.id,f"Hello, World! :3 >{b[0].tg_user_id}<")
 
 # askar's test some telebot functionality
 
