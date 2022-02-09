040a0c8aa35fbb84d37ed893b484a6a33f9b02f5
oqqa <oqqa.vw@gmail.com>
2022-02-09 18:54:25 +0400

    feat: show users counts

--- src/run.py
+++ src/run.py
@@ -6,13 +6,17 @@ from dotenv import load_dotenv
 load_dotenv('./.env')
 token = os.environ.get('TOKEN')
 
-a = DB_binding()
-b = a.Sql("SELECT * FROM users;")
+db = DB_binding()
+users_counts = len(db.Sql("SELECT * FROM users;"))
 
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id,f"Hello, World! :3 >{len(b)}<")
+  bot.send_message(message.chat.id,f"Hello, World! :3 \n Users counts: {users_counts}\n Your telegram id: {message.from_user.id}")
+  
+  if db.Get_id_from_tg_user_id(message.from_user.id) == 0:
+    db.Add_user(message.from_user.id, 0)
+    bot.send_message(message.chat.id,f"You added own database")
 
 # askar's test some telebot functionality
 
