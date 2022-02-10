befaacf0895cbcfcc2a9dab5deb933da2a4382d7
oqqa <oqqa.vw@gmail.com>
2022-02-10 09:42:31 +0400

    debug

--- src/bot_logic.py
+++ src/bot_logic.py
@@ -20,6 +20,14 @@ Users counts: {users_counts}
             self.send(tg_user_id,f"You added own database")
 
     def handler_message(self, tg_user_id, message):
-        self.send(tg_user_id, self.modify_msg(message))
+        modified_msg = self.modify_msg(message)
+
+        msg_start = ""
+        msg_end = ""
+        for row in modified_msg:
+            msg_start += row[0] + "\n\n"
+            msg_end += row[1] + "\n"
+        
+        self.send(tg_user_id, msg_start+msg_end)
 
     
