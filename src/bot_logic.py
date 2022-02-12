f68f69f9327eb2ce185df62d432192024850ff17
oqqa <oqqa.vw@gmail.com>
2022-02-12 11:47:01 +0400

    Debug

--- src/bot_logic.py
+++ src/bot_logic.py
@@ -80,7 +80,7 @@ class Bot_logic:
 
             if is_callback_button:
                 # time.sleep(0)
-                self.send(tg_user_id, f"---\nYou send:\n {message.get}\n---")
+                self.send(tg_user_id, f"---\nYou send:\n {message}\n---")
                 self.send(tg_user_id_companion, message)
                 self.db.Add_log(tg_user_id, session_id, message, time_send, "from_bot", 0)
             else: 
